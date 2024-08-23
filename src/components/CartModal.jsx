1cc1740ab9010a4fd37f8206e5cb5e40a33d04e9
Rahman <rahmansaif.413@gmail.com>
2024-08-24 01:08:17 +0600

    navbar added

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,11 +1,79 @@
 "use client"
+import Image from 'next/image';
 import React from 'react';
 
 const CartModal = () => {
+
+    const cartItems=true;
     return (
-        <div>
-            CartModal
-        </div>
+      <div className="w-max absolute p-4 rounded-md bg-white text-black top-12 right-0 flex flex-col gap-6 z-20">
+        {!cartItems ? (
+          <div className="">Cart is Empty</div>
+        ) : (
+          <>
+          <h2 className='text-xl'>Shopping Cart</h2>
+            <div className="flex flex-col gap-8">
+              <div className="flex gap-4">
+                <Image
+                  src="https://images.pexels.com/photos/13915944/pexels-photo-13915944.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                  width={72}
+                  height={96}
+                  className="object-cover rounded-md"
+                />
+                <div className="flex flex-col justify-between w-full">
+                  <div className="">
+                    <div className="flex items-center justify-between gap-8">
+                      <h2 className="font-semibold">Product Name</h2>
+                      <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                    </div>
+                    <div className="text-sm text-gray-50">available</div>
+                  </div>
+                  <div className="flex justify-between text-sm">
+                    <span className="text-gray-500">Qty .2</span>
+                    <span className="text-blue-500">Remove</span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <Image
+                  src="https://images.pexels.com/photos/13915944/pexels-photo-13915944.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                  width={72}
+                  height={96}
+                  className="object-cover rounded-md"
+                />
+                <div className="flex flex-col justify-between w-full">
+                  <div className="">
+                    <div className="flex items-center justify-between gap-8">
+                      <h2 className="font-semibold">Product Name</h2>
+                      <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                    </div>
+                    <div className="text-sm text-gray-50">available</div>
+                  </div>
+                  <div className="flex justify-between text-sm">
+                    <span className="text-gray-500">Qty .2</span>
+                    <span className="text-blue-500">Remove</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+                <div className="flex items-center justify-between font-semibold">
+                    <span className="">Subtotal</span>
+                    <span className="">$49</span>
+                </div>
+                <p className='text-gray-500 text-sm mt-2 mb-4'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+                <div className="flex justify-between text-sm">
+                    <button className='rounded-md py-3 px-4 ring-gray-300'>View Cart</button>
+                    <button className='rounded-md py-3 px-4 bg-black text-white'>Checkout</button>
+                </div>
+            </div>
+          </>
+        )}
+      </div>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,14 @@ import Navicons from './Navicons';
 const Navbar = () => {
     return (
       <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+        {/* small device */}
         <div className="flex items-center justify-between h-full md:hidden ">
           <Link href="/" className="text-2xl tracking-wide">
             Daraz
           </Link>
           <Menu></Menu>
         </div>
+        {/* mid & large device  */}
         <div className="hidden md:flex items-center justify-between gap-8 h-full">
           <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
             <Link href="/" className="flex items-center gap-3">
